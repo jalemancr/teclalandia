836d26645c81231a0c4153b68a909c0ecfacc238
jalemancr <jalemancr@gmail.com>
2025-10-26 15:08:33 -0600

    Inicializacion del Proyecto

---
+++ src/main/java/com/teclalandia/TeclalandiaApplication.java
@@ -0,0 +1,13 @@
+package com.teclalandia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeclalandiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeclalandiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teclalandia/TeclalandiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teclalandia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeclalandiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
